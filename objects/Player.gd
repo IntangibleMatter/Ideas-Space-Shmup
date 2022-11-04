fa015a1e98f617186fcb788309d3a8f2e554831b
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2022-11-03 22:34:33 -0700

    2022-11-03

--- objects/Player.gd
+++ objects/Player.gd
@@ -2,8 +2,15 @@ extends Area2D
 
 var speed = 100
 
+const laser = preload("res://objects/laser.tscn")
+
 func _process(delta):
 	if Input.is_action_pressed("ui_right"):
 		position.x += speed * delta
 	if Input.is_action_pressed("ui_left"):
 		position.x -= speed * delta
+	if Input.is_action_just_pressed("ui_accept"):
+		shoot()
+
+func shoot():
+	pass
